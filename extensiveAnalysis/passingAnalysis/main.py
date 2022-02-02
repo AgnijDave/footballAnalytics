7298341b7f385b8b0a029f8e4eaec09c68537e3b
Agnij <agnijd@gmail.com>
2022-02-03 00:21:57 +0530

    passing-analysis-merge-2

--- extensiveAnalysis/passingAnalysis/main.py
+++ extensiveAnalysis/passingAnalysis/main.py
@@ -235,3 +235,267 @@ avgPassesPer90 = playerWisedf["passesPer90"].mean()
 ## instead of passes per match
 playerWisedf[playerWisedf["passesPer90"] > avgPassesPer90].style.bar(subset=["passesPerMatch",
                                                                              "passesPer90"]).set_precision(1)
+
+
+"""# Generating and Analysing Pass Maps"""
+
+## getting the nodes and edges for plotting using mplsoccer ##
+## expand = True, gives a string instead of a list ##
+
+eventPassdf["startX"] = eventPassdf["location"].str.split(", ", expand=True)[0].str[1:].apply(pd.to_numeric)
+eventPassdf["startY"] = eventPassdf["location"].str.split(", ", expand=True)[1].str[:-1 ].apply(pd.to_numeric)
+
+eventPassdf["endX"] = eventPassdf["pass.end_location"].str.split(", ", expand=True)[0].str[1:].apply(pd.to_numeric)
+eventPassdf["endY"] = eventPassdf["pass.end_location"].str.split(", ", expand=True)[1].str[:-1].apply(pd.to_numeric)
+
+'''
+## based on matplotlib library ##
+
+pitch = mplsoccer.Pitch(pitch_color='#101010', line_color='white')
+fig, ax = pitch.draw(figsize=(14, 10)) ## width and height
+
+## generate arrows function, startx, starty, endx, endy
+arrows = pitch.arrows(eventPassdf["startX"], eventPassdf["startY"],
+                      eventPassdf["endX"], eventPassdf["endY"],
+                      ax=ax,
+                      width=.1,
+                      color="green")
+
+'''
+## get analysis on a particular match ## Atletico Madrid vs Barcelona [303696] ##
+eventPassdf[["match_id", "team.name"]].drop_duplicates()
+
+matchEventdf = eventPassdf[eventPassdf["match_id"] == 303696]
+pitch = mplsoccer.Pitch(pitch_color='#101010', line_color='white')
+fig, ax = pitch.draw(figsize=(14, 10))
+
+arrows = pitch.arrows(matchEventdf["startX"], matchEventdf["startY"],
+                      matchEventdf["endX"], matchEventdf["endY"],
+                      ax=ax,
+                      width=1,
+                      color="green")
+
+## analysis keeping segregating team passing into differet colors ##
+## Real Madrid vs Barcelona [303470] ##
+
+team_ids = eventPassdf[eventPassdf["match_id"] == 303470]["possession_team.id"].unique()
+
+# DF for Team 1:
+pass_map_df_team1 = eventPassdf[(eventPassdf["match_id"] == 303470)
+                                & (eventPassdf["possession_team.id"] == team_ids[0])]
+
+pitch = mplsoccer.Pitch(pitch_color='#101010', line_color='white')
+fig, ax = pitch.draw(figsize=(16, 8))
+arrows = pitch.arrows(pass_map_df_team1["startX"], pass_map_df_team1["startY"],
+                      pass_map_df_team1["endX"], pass_map_df_team1["endY"],
+                      ax=ax,
+                      width=1,
+                      color="green")
+ax.set_title(pass_map_df_team1["possession_team.name"].unique().item(), fontsize=30)
+
+
+# DF for Team 2:
+pass_map_df_team2 = eventPassdf[(eventPassdf["match_id"] == 303470)
+                                & (eventPassdf["possession_team.id"] == team_ids[1])]
+
+pitch = mplsoccer.Pitch(pitch_color='#101010', line_color='white')
+fig, ax = pitch.draw(figsize=(16, 8))
+arrows = pitch.arrows(pass_map_df_team2["startX"], pass_map_df_team2["startY"],
+                      pass_map_df_team2["endX"], pass_map_df_team2["endY"],
+                      ax=ax,
+                      width=1,
+                      color="#ba4f45")
+ax.set_title(pass_map_df_team2["possession_team.name"].unique().item(), fontsize=30)
+
+
+"""## Side-by-Side Pass Maps"""
+
+""" Set Pitch Parameters """
+pitch = mplsoccer.Pitch(pitch_color='#101010', line_color='white')
+""" Create 1x2 grid of subplots for Analysing two teams """
+
+## here axs is a list of vavlues since nrows and ncols has been provided ##
+fig, axs = pitch.draw(nrows=1, ncols=2, figsize=(16, 10))
+
+""" Team 1 Pass Map """
+# Draw arrows to create pass map
+arrows = pitch.arrows(pass_map_df_team1["startX"],
+                      pass_map_df_team1["startY"],
+                      pass_map_df_team1["endX"],
+                      pass_map_df_team1["endY"],
+                      ax=axs[0],
+                      width=1,
+                      color="green")
+# Set title for subplot
+axs[0].set_title(pass_map_df_team1["possession_team.name"].unique().item(),
+                 fontsize=30)
+
+""" Team 2 Pass Map """
+# Draw arrows to create pass map
+arrows = pitch.arrows(pass_map_df_team2["startX"],
+                      pass_map_df_team2["startY"],
+                      pass_map_df_team2["endX"],
+                      pass_map_df_team2["endY"],
+                      ax=axs[1],
+                      width=1,
+                      color="#ba4f45")
+# Set title for subplot
+axs[1].set_title(pass_map_df_team2["possession_team.name"].unique().item(),
+                 fontsize=30)
+
+
+
+"""
+Create grouped data for every match
+"""
+groups = eventPassdf.groupby(["match_id"])
+
+for name, group in groups:
+    print(name)
+    print(group.head()) ## will give the remainder of the data as a dictionary ##
+    break
+
+"""
+Following code will give pass maps for every match
+To Save all the plots to a pdf, uncomment line 363-365, 408, 410
+"""
+
+#import matplotlib.pyplot as plt
+#from matplotlib.backends.backend_pdf import PdfPages
+#my_pdf = PdfPages("./reports/passMapsLaLiga2019_20.pdf")
+'''
+for name, group in groups:
+#     print(name)
+    team_ids = group["possession_team.id"].unique()
+    # Bifurcate team-wise data #
+    # print(group.head())
+    pass_map_df_team1 = deepcopy(group[(group["possession_team.id"] == team_ids[0])])
+    pass_map_df_team2 = deepcopy(group[(group["possession_team.id"] == team_ids[1])])
+    
+    ## Here onwards the code is picked up from above ##
+    ## Ideally a function ccan be created for this purpose ##
+    
+    # Create 1x2 grid of subplots for Analysing two teams #
+    fig, axs = pitch.draw(nrows=1, ncols=2, figsize=(16, 10))
+
+    # Team 1 Pass Map #
+    # Draw arrows to create pass map
+    arrows = pitch.arrows(pass_map_df_team1["startX"],
+                          pass_map_df_team1["startY"],
+                          pass_map_df_team1["endX"],
+                          pass_map_df_team1["endY"],
+                          ax=axs[0],
+                          width=1,
+                          color="#ba4f45")
+    # Set title for subplot
+    axs[0].set_title(pass_map_df_team1["possession_team.name"].unique().item(),
+                     fontsize=30)
+
+    # Team 2 Pass Map #
+    # Draw arrows to create pass map
+    arrows = pitch.arrows(pass_map_df_team2["startX"],
+                          pass_map_df_team2["startY"],
+                          pass_map_df_team2["endX"],
+                          pass_map_df_team2["endY"],
+                          ax=axs[1],
+                          width=1,
+                          color="#ad993c")
+    # Set title for subplot
+    axs[1].set_title(pass_map_df_team2["possession_team.name"].unique().item(),
+                     fontsize=30)
+    
+    fig.show()
+    #my_pdf.savefig()
+
+#my_pdf.close()
+
+'''
+
+barcaData = eventPassdf[eventPassdf["team.id"] == 217]
+
+## This will give a count of passes, as seen index will be passer and columns will be receivers ##
+## count of type.id gives the number of passes ##
+## can use player.id and pass.recipient.id to avoid any challenges arising due to similar names ##
+
+barcaPassMatrix = barcaData.pivot_table(values="type.id", index="player.name",
+                                        columns="pass.recipient.name", aggfunc="count")
+
+## following will highlight max value for each column i.e. axis=0 *max no. of passes received ##
+## axis = 1, will give max value for each row *max no. of passes created  ##
+## here axis properties are opposite than normal  ## 
+
+barcaPassMatrix.style.highlight_max(axis=0).set_precision(0)
+
+## interpret results based on color shading, by default axis = 0 i.e. columns ##s
+barcaPassMatrix.style\
+    .background_gradient(cmap="Blues")\
+    .highlight_null('black').set_precision(0)
+
+
+## following snippet gives the entire df view that is interactive on hovering ##
+"""
+# Go to [pandas styling page](https://pandas.pydata.org/pandas-docs/stable/user_guide/style.html) and search for "Fun Stuff
+## td tabledata, tr tablerows, th tableheaders ##
+barcaPassMatrix.style\
+    .background_gradient(cmap="Blues", axis=1)\
+    .highlight_null('black').set_precision(0)\
+    .set_table_styles([dict(selector="th",
+                            props=[("font-size", "4pt")]),
+                       dict(selector="td",
+                            props=[('padding', "0em 0em")]),
+                       dict(selector="th:hover",
+                            props=[("font-size", "12pt")]),
+                       dict(selector="tr:hover td:hover",
+                            props=[('max-width', '200px'),
+                                   ('font-size', '15pt')])])
+
+## following heatmap using seaborn library, pandas gives option for row, col but sns acccounts for all values in df ##
+
+plt.figure(figsize=(18, 12))
+sns.heatmap(data=barcaPassMatrix, annot=True, linewidths=1, fmt=".0f", cmap="viridis")
+plt.xlabel("")
+plt.ylabel("") """
+
+### El-Classico
+
+
+eventPassdf.loc[eventPassdf["team.name"].str.contains("Madrid"), ["team.name", "team.id"]].drop_duplicates()
+
+## seasonMetaDataLaLiga2019 contains data for multiple seasons hence season_id is required ##
+seasonMetaDataLaLiga2019[["season.season_id", "season.season_name"]].drop_duplicates()
+seasonMetaDataLaLiga2019[(seasonMetaDataLaLiga2019["season.season_id"] == 42) &
+                         (seasonMetaDataLaLiga2019["home_team.home_team_id"].isin([217, 220])) &
+                         (seasonMetaDataLaLiga2019["away_team.away_team_id"].isin([217, 220]))]
+
+#eventPassdf[eventPassdf["match_id"].isin([303596, 303470])]["match_id"].drop_duplicates()
+#eventPassdf[eventPassdf["match_id"].isin([303596, 303470])]["match_id"].drop_duplicates()
+elClassicoData = eventPassdf[eventPassdf["match_id"].isin([303596, 303470])]
+
+elClassicoPassMatrix = elClassicoData.pivot_table(values="type.id", index="player.name",
+                                                  columns="pass.recipient.name", aggfunc="count")
+
+elClassicoPassMatrix.style\
+    .background_gradient(cmap="Blues", axis=1)\
+    .highlight_null('black').set_precision(0)\
+    .set_table_styles([dict(selector="th",
+                            props=[("font-size", "4pt")]),
+                       dict(selector="td",
+                            props=[('padding', "0em 0em")]),
+                       dict(selector="th:hover",
+                            props=[("font-size", "12pt")]),
+                       dict(selector="tr:hover td:hover",
+                            props=[('max-width', '200px'),
+                                   ('font-size', '15pt')])])
+'''
+plt.figure(figsize=(18, 12))
+sns.heatmap(data=elClassicoPassMatrix, annot=True, linewidths=1, fmt=".0f", cmap="YlGnBu")
+plt.xlabel("")
+plt.ylabel("")
+'''
+
+## checking data for players having passes a minimum of 10 passes ##
+plt.figure(figsize=(18, 12))
+sns.heatmap(data=elClassicoPassMatrix[elClassicoPassMatrix.apply(lambda x: x > 10)],
+            annot=True, linewidths=1, fmt=".0f", cmap="YlGnBu")
+plt.xlabel("")
+plt.ylabel("")
